c165876c6a63b1fae3d48bc3d8798409454f43ce
Antonio Coppola <antonio.coppola@linksmt.it>
2018-10-16 11:31:18 +0200

    fix

--- src/android/FingerprintAuthAux.java
+++ src/android/FingerprintAuthAux.java
@@ -622,7 +622,7 @@ public class FingerprintAuthAux {
                     + e.getMessage();
             if(message == "Key user not authenticated"){
                 removePermanentlyInvalidatedKey();
-                errorMessage = "KeyPermanentlyInvalitadedException"
+                errorMessage = "KeyPermanentlyInvalitadedException";
             }
             Log.e(TAG, errorMessage);
         }
